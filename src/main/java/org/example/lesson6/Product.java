478dfb9f56c49d56664f5da1bf0d1b0520ac6237
Ivan Lysiak <ivan.l@youcontrol.com.ua>
2023-10-17 20:46:35 +0300

    lesson6_Test_1

---
+++ src/main/java/org/example/lesson6/Product.java
@@ -0,0 +1,61 @@
+package org.example.lesson6;
+
+public class Product {
+  private String brand;
+  private int article;
+  private double price;
+  private String description;
+  private boolean existence;
+
+  public Product(String brand, int article, double price, String description, boolean existence) {
+    this.brand = brand;
+    this.article = article;
+    this.price = price;
+    this.description = description;
+    this.existence = existence;
+  }
+  // Метод Гет для отримання значення полів
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public int getArticle() {
+    return article;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public boolean getExistence() {
+    return existence;
+  }
+
+  // Метод Сетт для зміни значення полів
+  public void setBrand(String newBrand) {
+    this.brand = newBrand;
+
+  }
+
+  public void setArticle(int newArticle) {
+    this.article = newArticle;
+  }
+
+  public void setPrice(double newPrice) {
+    this.price = newPrice;
+  }
+
+  public void setDescription(String newDescription) {
+    this.description = newDescription;
+  }
+
+  public void setExistence(boolean newExistence) {
+    this.existence = newExistence;
+  }
+
+}
